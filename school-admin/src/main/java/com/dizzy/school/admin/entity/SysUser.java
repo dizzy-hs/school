754d7766182cf7716c60a401431165ba4e40528e
dizzy-hs <hanshanemail@163.com>
2025-01-15 15:11:09 +0000

    新增SysUser 2025年1月15日23点11分

---
+++ school-admin/src/main/java/com/dizzy/school/admin/entity/SysUser.java
@@ -0,0 +1,38 @@
+package com.dizzy.school.admin.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GenerationType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "sys_user")
+public class SysUser {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    private String password;
+
+    private Boolean isValid;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private Boolean isDeleted;
+}
